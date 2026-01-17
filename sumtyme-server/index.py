49dd3163a1c4b3f77ef5830c3c9977380b213f44
swissdagger <mustapha.sadiq@sumtyme.ai>
2026-01-17 17:57:38 +0000

    Create index.py

---
+++ sumtyme-server/index.py
@@ -0,0 +1,8 @@
+# File: sumtyme_server/index.py
+
+# We import the 'app' object from your main.py file.
+# Note the underscore in the folder name!
+from sumtyme_server.main import app
+
+# This part allows Vercel to manage the running of the app
+# No other code is strictly needed here.
